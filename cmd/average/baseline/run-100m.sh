5522f2acee83684b3dec717d32178d350e95d20d
Simon Kinsella <simon.kinsella@gmail.com>
2024-02-27 21:27:24 +0000

    Adding helper script and sample output

---
+++ cmd/average/baseline/run-100m.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+set -e
+INPUT=measurements-100m.txt
+
+go build -o calculate-temps .
+go version
+echo input: ${INPUT}
+
+/usr/bin/time -p sh -c './calculate-temps -in ../../../datasets/'${INPUT}' > output.txt' | grep real
